cdc96396d03908e81f63a306356eb154c25f03c3
nafimbrk <nafimubarok2005@gmail.com>
2025-01-30 00:31:47 +0700

    kurang update ama delete

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        //get all posts from Models
+        $posts = Post::latest()->get();
+
+        //return view with data
+        return view('posts', compact('posts'));
+    }
+
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create post
+        $post = Post::create([
+            'title'     => $request->title, 
+            'content'   => $request->content
+        ]);
+
+        //return response
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Berhasil Disimpan!',
+            'data'    => $post  
+        ]);
+    }
+}
